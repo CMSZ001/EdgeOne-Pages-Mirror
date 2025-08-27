7083290e753c269123a06bc597f98674cfb697f4
CMSZ <git@acmsz.top>
2025-08-27 13:34:39 +0800

    fix(tur): 修改路由逻辑直接代理到镜像站点而非使用静态路由
    将 /tur/ 路径的请求直接代理到 tur-mirror.pages.dev 而不是使用 context.next() 的静态路由
    添加 no-store 缓存控制头确保不缓存响应

--- functions/tur/index.js
+++ functions/tur/index.js
@@ -1,12 +1,21 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  // 仅在没有斜杠时重定向
+  // 只有在没有斜杠时重定向
   if (url.pathname === "/tur") {
     url.pathname = "/tur/";
     return Response.redirect(url.toString(), 301);
   }
 
-  // 如果已经是 /tur/，就交给默认的 Pages 静态路由或者 tur/[[...path]].js
-  return context.next();
+  // 如果是 /tur/，直接代理到 tur-mirror，不要用 context.next()
+  if (url.pathname === "/tur/") {
+    const resp = await fetch("https://tur-mirror.pages.dev/");
+    return new Response(resp.body, {
+      status: resp.status,
+      headers: {
+        ...resp.headers,
+        "Cache-Control": "no-store", // 不缓存
+      },
+    });
+  }
 }
