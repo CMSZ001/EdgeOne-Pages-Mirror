e0e499345302d0506bac018fcd7254a69f12de7a
CMSZ <git@acmsz.top>
2025-08-27 14:28:34 +0800

    refactor(functions): 移除tur重定向功能
    该功能已不再需要，因此删除对应的处理函数

--- functions/tur/index.js
+++
@@ -1,5 +0,0 @@
-// 仅处理 /tur → /tur/
-export default async function (context) {
-  const location = new URL('/tur/', context.request.url); // 真实域名由平台自动补上
-  return Response.redirect(location.pathname, 301);
-}
