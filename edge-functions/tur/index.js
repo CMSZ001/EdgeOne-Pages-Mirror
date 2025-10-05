d37471e23dfd18d680ab73a91c72abb6bbb7c2b4
CMSZ001 <130162476+CMSZ001@users.noreply.github.com>
2025-10-05 15:26:50 +0800

    Update index.js

--- edge-functions/tur/index.js
+++ edge-functions/tur/index.js
@@ -7,8 +7,18 @@ export async function onRequestGet(context) {
   if (path.includes("..")) return new Response("Bad Request", { status: 400 });
 
   if (path === "/tur") return Response.redirect(url.origin + "/tur/", 301);
-  if (path === "/tur/") return fetch("https://tur-mirror.pages.dev/");
-}
+  if (path === "/tur/") {
+    return fetch("https://tur-mirror.pages.dev/", {
+      version: "HTTP/2.0",
+      eo: {
+        timeoutSetting: {
+          connectTimeout: 300000,
+          readTimeout: 300000,
+          writeTimeout: 300000
+        }
+      }
+    });
+  }
 
 export async function onRequestHead(context) {
   return onRequestGet(context);
